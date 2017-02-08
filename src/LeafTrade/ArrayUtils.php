35bd4d412e43ac19dd4d35061f2587963f74b93f
Trevor Zablocki <trevor@splt.io>
2017-02-08 05:07:16 +0000

    Add reverse array method

--- src/LeafTrade/ArrayUtils.php
+++ src/LeafTrade/ArrayUtils.php
@@ -9,5 +9,16 @@ namespace LeafTrade;
 
 class ArrayUtils
 {
-
+    /*
+     * Takes a string, strips non-alphanumeric characters, and reverses the resulting array of words
+     *
+     * @param (string) The string to be manipulated
+     * @return (reversedArray) The resulting flipped array of words
+     */
+    public static function reverseArray($string)
+    {
+        $strippedString = preg_replace("/[^A-Za-z0-9 ]/", "", $string);
+        $arrOfWords = explode(' ', $strippedString);
+        return array_reverse($arrOfWords);
+    }
 }
